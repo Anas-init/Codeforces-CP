669ea55b3578d154a8fcd715a2e6f707183d5018
Anas-init <anastech900@gmail.com>
2024-10-04 02:53:51 -0700

    new sol. added

---
+++ codeforces_974_div3/c.cpp
@@ -0,0 +1,110 @@
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+#define fixed(n) fixed << setprecision(n)
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
+#define all(v1) v1.begin(), v1.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define fi first
+#define se second
+#define ll long long
+#define ull unsigned long long
+#define EPS 1e-9
+constexpr int INF = 1 << 30, Mod = 1e9 + 7;
+constexpr ll LINF = 1LL << 62;
+#define PI acos(-1)
+template < typename T = int > using Pair = pair < T, T >;
+vector < string > RET = {"NO", "YES"};
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << endl; 
+    return out;
+}
+
+ll Solve(ll n,vector < ll> v1){
+    if (v1.size() ==1 || ( v1.size() ==2 )) return -1;
+    sort(all(v1));
+    ll mid=v1[n/2];
+    ll sum=0;
+    for (auto &x : v1){
+        sum+=x;
+    }
+    ll x=((n*2)* (mid));
+    return ( 0 > x-sum+1? 0  : x-sum+1);
+}
+
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int test_cases ;
+    cin>>test_cases;
+    vector<ll> v1;
+    for(int tc = 1; tc <= test_cases; tc++){
+        ll n;
+        cin>>n;
+        vector<ll> v2(n);
+        cin>>v2;
+        v1.push_back(Solve(n,v2));
+    }
+    cout<<v1;
+    return 0;
+}
